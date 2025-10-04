835679cb02f18ac9de579618375c20655197a9b0
paulwinex <paulwinex@gmail.com>
2025-10-04 21:50:52 +0300

    catch before_start event

---
+++ agio_drive/plugins/callbacks/pipe_callbacks.py
@@ -0,0 +1,10 @@
+from agio.core.events import callback
+
+
+@callback('pipe.publish.before_start', raise_error=True)
+def on_before_start_publish(event, payload = None):
+    # check drive is started
+    print(' CHECK DRIVE IS STARED ')
+    from agio_drive.utils import drive_app
+    if not drive_app.is_active():
+        raise Exception('Drive Client is not active')
