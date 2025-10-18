fcdba258eb976a45a2b0789c6d74d8863fcdbec7
paulwinex <paulwinex@gmail.com>
2025-10-18 20:31:11 +0300

    show error dialog if drive is not active

--- agio_drive/plugins/callbacks/pipe_callbacks.py
+++ agio_drive/plugins/callbacks/pipe_callbacks.py
@@ -1,4 +1,5 @@
 from agio.core.events import callback, AEvent
+from agio.tools.qt import show_message_dialog
 
 
 @callback('pipe.publish.before_start', raise_error=True)
@@ -7,4 +8,5 @@ def on_before_start_publish(event: AEvent):
     # get checking is enabled from settings
     from agio_drive.utils import drive_app
     if not drive_app.is_active():
+        show_message_dialog('Drive Client is not active', 'Error', 'error')
         raise Exception('Drive Client is not active')
