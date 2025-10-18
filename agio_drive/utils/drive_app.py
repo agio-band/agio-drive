0d491a8c858bebbb257c5993a7f9028eaa0973f2
paulwinex <paulwinex@gmail.com>
2025-10-18 21:00:15 +0300

    added get_mount_paths util

--- agio_drive/utils/drive_app.py
+++ agio_drive/utils/drive_app.py
@@ -1,10 +1,18 @@
 import requests
 
+BASE_URL = 'http://localhost:9085'
+
 
 def is_active():
     try:
-        requests.get('http://localhost:9085/swagger/index.html', timeout=1)
+        requests.get(f'{BASE_URL}/swagger/index.html', timeout=1)
         return True
     except (requests.exceptions.ConnectionError, requests.exceptions.ConnectTimeout) as e:
         print(f'{e.__class__.__module__}.{e.__class__.__name__}:', e)
         return False
+
+
+def get_mount_paths():
+    if not is_active():
+        return
+    return requests.get(f'{BASE_URL}/mount', timeout=1).json()
